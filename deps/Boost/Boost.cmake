2312070738991f8fa5ab34c8364e10144b151a13
tamasmeszaros <meszaros.q@gmail.com>
2023-07-27 14:24:50 +0200

    fix typo in boost download url

--- deps/Boost/Boost.cmake
+++ deps/Boost/Boost.cmake
@@ -1,5 +1,5 @@
 add_cmake_project(Boost
-    URL "hhttps://github.com/boostorg/boost/releases/download/boost-1.82.0/boost-1.82.0.zip"
+    URL "https://github.com/boostorg/boost/releases/download/boost-1.82.0/boost-1.82.0.zip"
     URL_HASH SHA256=200f9292b5ef957ab551a648834239f502df165cb7bff18432702fb7ae98accb
     CMAKE_ARGS
         -DBOOST_INCLUDE_LIBRARIES:STRING=beast
