ac97c1d156de503680fa8a95562b28b2b42e03b7
tamasmeszaros <meszaros.q@gmail.com>
2023-09-19 16:15:50 +0200

    Fix wasm build and don't build pybind11 tests

--- deps/pybind11/pybind11.cmake
+++ deps/pybind11/pybind11.cmake
@@ -8,4 +8,6 @@ add_cmake_project(pybind11
     EXCLUDE_FROM_ALL ${_exclude_from_all}
     URL https://github.com/pybind/pybind11/archive/refs/tags/v2.11.1.zip
     URL_HASH SHA256=b011a730c8845bfc265f0f81ee4e5e9e1d354df390836d2a25880e123d021f89
+    CMAKE_ARGS
+        -DPYBIND11_TEST:BOOL=OFF
 )
