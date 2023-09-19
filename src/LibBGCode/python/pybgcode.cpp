2a58f3b77c0efc8f02c620c715e8b67d497745b8
tamasmeszaros <meszaros.q@gmail.com>
2023-09-19 13:14:05 +0200

    build python binding for linux and msvc
    fix previous github yaml
    
    tryfix gh workflow 2
    
    tryfix gh workflow 3
    
    
    Python binding seems to work

--- src/LibBGCode/python/pybgcode.cpp
+++ src/LibBGCode/python/pybgcode.cpp
@@ -1,8 +1,24 @@
+#include <cstdio>
 #include <pybind11/pybind11.h>
 
 #include "convert/convert.hpp"
 
 namespace py = pybind11;
+
+bgcode::binarize::BinarizerConfig get_config()
+{
+    bgcode::binarize::BinarizerConfig config;
+    config.checksum = bgcode::core::EChecksumType::CRC32;
+    config.compression.file_metadata = bgcode::core::ECompressionType::None;
+    config.compression.print_metadata = bgcode::core::ECompressionType::None;
+    config.compression.printer_metadata = bgcode::core::ECompressionType::None;
+    config.compression.slicer_metadata = bgcode::core::ECompressionType::Deflate;
+    config.compression.gcode = bgcode::core::ECompressionType::Heatshrink_12_4;
+    config.gcode_encoding = bgcode::core::EGCodeEncodingType::MeatPackComments;
+    config.metadata_encoding = bgcode::core::EMetadataEncodingType::INI;
+
+    return config;
+}
 
 PYBIND11_MODULE(pybgcode, m) {
     m.doc() = R"pbdoc(
@@ -48,11 +64,51 @@ PYBIND11_MODULE(pybgcode, m) {
         .value("AlreadyBinarized", bgcode::core::EResult::AlreadyBinarized)
         ;
 
-    m.def("from_ascii_to_binary", &bgcode::convert::from_ascii_to_binary, R"pbdoc(
-        Convert ascii gcode to binary format
-    )pbdoc");
+    py::enum_<bgcode::core::ECompressionType>(m, "BGCode_CompressionType")
+        .value("none", bgcode::core::ECompressionType::None)
+        .value("Deflate", bgcode::core::ECompressionType::Deflate)
+        .value("Heatshrink_11_4", bgcode::core::ECompressionType::Heatshrink_11_4)
+        .value("Heatshrink_12_4", bgcode::core::ECompressionType::Heatshrink_12_4);
+    py::enum_<bgcode::core::EGCodeEncodingType>(m, "BGCode_GCodeEncodingType")
+        .value("none", bgcode::core::EGCodeEncodingType::None)
+        .value("MeatPack", bgcode::core::EGCodeEncodingType::MeatPack)
+        .value("MeatPackComments", bgcode::core::EGCodeEncodingType::MeatPackComments);
+    py::enum_<bgcode::core::EMetadataEncodingType>(m, "BGCode_MetadataEncodingType")
+        .value("INI", bgcode::core::EMetadataEncodingType::INI);
+    py::enum_<bgcode::core::EChecksumType>(m, "BGCode_ChecksumType")
+        .value("none", bgcode::core::EChecksumType::None)
+        .value("CRC32", bgcode::core::EChecksumType::CRC32);
 
-    m.def("from_binary_to_ascii", &bgcode::convert::from_ascii_to_binary, R"pbdoc(
+    py::class_<bgcode::binarize::BinarizerConfig::Compression>(m, "BGCode_BinarizerCompression")
+        .def_readwrite("file_metadata", &bgcode::binarize::BinarizerConfig::Compression::file_metadata)
+        .def_readwrite("printer_metadata", &bgcode::binarize::BinarizerConfig::Compression::printer_metadata)
+        .def_readwrite("print_metadata", &bgcode::binarize::BinarizerConfig::Compression::print_metadata)
+        .def_readwrite("slicer_metadata", &bgcode::binarize::BinarizerConfig::Compression::slicer_metadata)
+        .def_readwrite("gcode", &bgcode::binarize::BinarizerConfig::Compression::gcode);
+    py::class_<bgcode::binarize::BinarizerConfig>(m, "BGCode_BinarizerConfig")
+        .def_readwrite("compression", &bgcode::binarize::BinarizerConfig::compression)
+        .def_readwrite("gcode_encoding", &bgcode::binarize::BinarizerConfig::gcode_encoding)
+        .def_readwrite("metadata_encoding", &bgcode::binarize::BinarizerConfig::metadata_encoding)
+        .def_readwrite("checksum", &bgcode::binarize::BinarizerConfig::checksum);
+
+    m.def("get_config", &get_config);
+
+    m.def("fopen", [](const char * name, const char *mode) {
+        FILE * fptr = std::fopen(name, mode);
+
+        return py::capsule(fptr, "FILE*");
+    });
+
+    m.def("fclose", [](py::capsule fileptr) { std::fclose(fileptr.get_pointer<FILE>()); });
+
+    m.def("from_ascii_to_binary", [](py::capsule infile, py::capsule outfile, const bgcode::binarize::BinarizerConfig &config) {
+            return bgcode::convert::from_ascii_to_binary(*infile.get_pointer<FILE>(), *outfile.get_pointer<FILE>(), config); },
+        R"pbdoc(Convert ascii gcode to binary format)pbdoc"
+    );
+
+    m.def("from_binary_to_ascii", [] (py::capsule infile, py::capsule outfile, bool verify_checksum) {
+            return bgcode::convert::from_binary_to_ascii(*infile.get_pointer<FILE>(), *outfile.get_pointer<FILE>(), verify_checksum);
+        } , R"pbdoc(
         Convert binary gcode to textual format
     )pbdoc");
 
--- tests/pybgcode/test_convert.py
+++ tests/pybgcode/test_convert.py
@@ -1,17 +1,24 @@
 import pybgcode
-import ctypes
 
 def test_main():
-    print(pybgcode.__version__)
+    # print(pybgcode.__version__)
     # assert pybgcode.__version__ == "0.0.1"
 
-    in_f = open("/home/quarky/Workspace/prusa3d/libbingcode/libbgcode-src-master/tests/data/mini_cube_b_ref.gcode", "r");
-    out_f = open("mini_cube_b_ref.bgcode", "w");
-    
+    in_f = pybgcode.fopen("mini_cube_b_ref.gcode", "r");
+    out_f = pybgcode.fopen("mini_cube_b_ref.bgcode", "w");
+
     assert(in_f)
     assert(out_f)
-    res = pybgcode.from_ascii_to_binary(ctypes.convert_file(in_f), ctypes.convert_file(out_f));
+
+    cfg = pybgcode.get_config();
+    cfg.compression.file_metadata = pybgcode.BGCode_CompressionType.Heatshrink_11_4
+    res = pybgcode.from_ascii_to_binary(in_f, out_f, cfg);
     print(res)
+
+    assert(res == pybgcode.EResult.Success)
+
+    pybgcode.fclose(in_f)
+    pybgcode.fclose(out_f)
 
 if __name__ == '__main__':
     test_main()
