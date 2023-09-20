ee3c9bcfa8d4b31405d0964b1685820401da61d6
tamasmeszaros <meszaros.q@gmail.com>
2023-09-20 18:33:32 +0200

    got python-cmake-wheel working

--- deps/python-cmake-wheel/python-cmake-wheel.cmake
+++ deps/python-cmake-wheel/python-cmake-wheel.cmake
@@ -1,11 +1,19 @@
 include(ExternalProject)
 
+set(_exclude_from_all OFF)
+
+if (NOT ${PROJECT_NAME}_SELECT_pybind11)
+  set(_exclude_from_all ON)
+endif ()
+
 ExternalProject_Add(dep_python_cmake_wheel
+    EXCLUDE_FROM_ALL ${_exclude_from_all}
+    
     URL https://github.com/Klebert-Engineering/python-cmake-wheel/archive/refs/tags/v0.9.0.zip
 
     DOWNLOAD_DIR        ${DEP_DOWNLOAD_DIR}/${projectname}
-
-    CONFIGURE_COMMAND
-    BUILD_COMMAND
-    INSTALL_COMMAND ${CMAKE_COMMAND} -E copy *.* ${DESTDIR}/usr/local/share/cmake/python_cmake_wheel
+    BUILD_IN_SOURCE ON
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory .  ${DESTDIR}/usr/local/share/cmake/python_cmake_wheel
 )
