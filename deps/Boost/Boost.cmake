5a25ea65131a3abb551af2f10659547ab1de8168
MichaIng <micha@dietpi.com>
2025-04-30 15:42:37 +0200

    Bump boost to v1.88.0
    to fix cmake_minimum_required statements with versions incompatible with recent cmake.
    
    Signed-off-by: MichaIng <micha@dietpi.com>

--- deps/Boost/Boost.cmake
+++ deps/Boost/Boost.cmake
@@ -1,6 +1,6 @@
 add_cmake_project(Boost
-    URL "https://github.com/boostorg/boost/releases/download/boost-1.82.0/boost-1.82.0.zip"
-    URL_HASH SHA256=200f9292b5ef957ab551a648834239f502df165cb7bff18432702fb7ae98accb
+    URL "https://github.com/boostorg/boost/releases/download/boost-1.88.0/boost-1.88.0-cmake.zip"
+    URL_HASH SHA256=68cf5e488c9e6315ee101460b216cc0da6a401e527ff53446122d8c047a8fab3
     LIST_SEPARATOR |
     CMAKE_ARGS
         -DBOOST_INCLUDE_LIBRARIES:STRING=beast|nowide
